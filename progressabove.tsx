4bbc155a9e09b2688ff4898c9ef9081d22d044fb
v15hv4 <vishva2912@gmail.com>
2022-03-01 22:44:58 +0530

    Reduce to 3 layers

---
+++ progressabove.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="progressabove" tilewidth="8" tileheight="8" tilecount="39936" columns="192">
+ <image source="progressabove.png" width="1536" height="1664"/>
+</tileset>
---
+++ progressworld.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="progressworld" tilewidth="8" tileheight="8" tilecount="39936" columns="192">
+ <image source="progressworld.png" width="1536" height="1664"/>
+</tileset>
