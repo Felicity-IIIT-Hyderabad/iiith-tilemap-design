ddf3683cde6b87e238b15451222b0953b9a9aa52
v15hv4 <vishva2912@gmail.com>
2022-03-14 05:28:10 +0530

    Fix map proportions, replace base texture

---
+++ 4_layer_gameplay_optimization/more_tweaks.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="more_tweaks" tilewidth="8" tileheight="8" tilecount="24424" columns="172">
+ <image source="more_tweaks.png" width="1376" height="1136"/>
+</tileset>
